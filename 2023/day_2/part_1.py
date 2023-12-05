ebd0a5cf8c8c9085d525e7528057bb2f2ddb7eab
HarryEP <44836842+HarryEP@users.noreply.github.com>
2023-12-05 14:40:10 +0000

    basic structure

--- 2023/day_2/part_1.py
+++ 2023/day_2/part_1.py
@@ -4,6 +4,15 @@ def read_file(file_name):
         data = file.read()
     return data.split('\n')
 
+def is_game_valid() -> bool:
+    pass
+
+def count_valid_games(data: list[str]) -> count:
+    count = 0
+    for games in data
+    if is_game_valid:
+        count += 1
 
 if __name__ == "__main__":
     data = read_file('sample.txt')
+    print(count_valid_games(data))
