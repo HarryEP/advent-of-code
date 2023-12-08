5905991f3e5d972fcb7c6a1c99f47aeea6f51299
HarryEP <44836842+HarryEP@users.noreply.github.com>
2023-12-08 21:12:17 +0000

    created a 2d-array grid

--- 2023/day_3/part_1.py
+++ 2023/day_3/part_1.py
@@ -5,13 +5,26 @@ def read_file(file_name):
     return data.split('\n')
 
 
+def get_grid(data: list[str]) -> list[list[str]]:
+    grid = []
+    for line in data:
+        new_line = []
+        for char in line:
+            new_line.append(char)
+        grid.append(new_line)
+    return grid
+
+
 def get_part_numbers() -> list[int]:
     pass
 
 
-def total_part_numbers():
+def total_part_numbers(part_numbers: list[int]):
     pass
 
 
 if __name__ == "__main__":
     print(len(read_file("day_3/sample.txt")[0]))
+    data = read_file("day_3/sample.txt")
+    grid = get_grid(data)
+    print(grid)
