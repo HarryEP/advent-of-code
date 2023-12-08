6683325f09121d5f878d980cee9bf05828479c50
HarryEP <44836842+HarryEP@users.noreply.github.com>
2023-12-08 20:26:29 +0000

    checked that the input is a 140x140 grid

--- 2023/day_3/part_1.py
+++ 2023/day_3/part_1.py
@@ -3,3 +3,15 @@ def read_file(file_name):
     with open(file_name) as file:
         data = file.read()
     return data.split('\n')
+
+
+def get_part_numbers() -> list[int]:
+    pass
+
+
+def total_part_numbers():
+    pass
+
+
+if __name__ == "__main__":
+    print(len(read_file("day_3/sample.txt")[0]))
