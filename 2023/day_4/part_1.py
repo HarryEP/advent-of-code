ae19801896b7012785fbd9a5cf06a54796c0ebdd
HarryEP <44836842+HarryEP@users.noreply.github.com>
2023-12-08 21:40:40 +0000

    added a way to total up values

--- 2023/day_4/part_1.py
+++ 2023/day_4/part_1.py
@@ -5,5 +5,21 @@ def read_file(file_name):
     return data.split('\n')
 
 
+def scratchcard_value(scratchcard: str) -> int:
+    winning_numbers, card_numbers = scratchcard.split("|")
+    print(winning_numbers)
+    print(card_numbers)
+    return 0
+
+
+def sum_up_scratchcards(data: list[str]) -> int:
+    total = 0
+    for scratchcard in data:
+        card = scratchcard.split(":")[1]
+        total += scratchcard_value(card)
+    return total
+
+
 if __name__ == "__main__":
-    print("solve scratchcards")
+    data = read_file("day_4/sample.txt")
+    print(sum_up_scratchcards(data))
