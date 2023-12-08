4282116c93b2a25a03c10748ad8eadfbd19142ee
HarryEP <44836842+HarryEP@users.noreply.github.com>
2023-12-08 21:22:52 +0000

    day 4 set up

---
+++ 2023/day_4/part_1.py
@@ -0,0 +1,9 @@
+def read_file(file_name):
+    '''file to read file with file_name as given'''
+    with open(file_name) as file:
+        data = file.read()
+    return data.split('\n')
+
+
+if __name__ == "__main__":
+    print("solve scratchcards")
