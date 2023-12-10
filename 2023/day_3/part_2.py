ce8296115c8d9ea30ebd0e717fec00f901712073
HarryEP <44836842+HarryEP@users.noreply.github.com>
2023-12-10 23:28:46 +0000

    work in progress

--- 2023/day_3/part_2.py
+++ 2023/day_3/part_2.py
@@ -4,6 +4,8 @@ from part_1 import read_file, create_grid
 def get_gear_numbers(grid: list[list[str]], size: int) -> list[int]:
     '''puts all the gear numbers in one list'''
     gear_numbers = []
+    star_locations = []
+    star_numbers = {}
     for y in range(size):
         number = ''
         star = False
@@ -11,7 +13,8 @@ def get_gear_numbers(grid: list[list[str]], size: int) -> list[int]:
             if grid[y][x].isdigit() and x == (size - 1):
                 number += grid[y][x]
                 if not star:
-                    star = check_for_star(x, y, grid, star, size-1)
+                    star, star_locations = check_for_star(
+                        x, y, grid, star, size-1, star_locations, star_numbers)
                 if star:
                     gear_numbers.append(int(number))
                 star = False
@@ -19,34 +22,51 @@ def get_gear_numbers(grid: list[list[str]], size: int) -> list[int]:
             elif grid[y][x].isdigit():
                 number += grid[y][x]
                 if not star:
-                    star = check_for_star(x, y, grid, star, size-1)
+                    star, star_locations = check_for_star(
+                        x, y, grid, star, size-1, star_locations, star_numbers)
             else:
                 if star:
-                    gear_numbers.append(int(number))
+                    gear_number = check_if_gear_number(
+                        star_locations, star_numbers)
+                    if gear_number != 0:
+                        gear_numbers.append()
                 star = False
                 number = ''
     return gear_numbers
 
 
-def check_for_star(x: int, y: int, grid: list[list[str]], symbol: bool, maximum: int) -> bool:
+def check_if_gear_number(star_locations, star_numbers):
+    pass
+
+
+def check_for_star(x: int, y: int, grid: list[list[str]], symbol: bool, maximum: int,
+                   star_locations: list, star_numbers: dict) -> bool:
     '''checks if a star borders the digit selected'''
     if x != 0 and y != 0 and grid[y-1][x-1] == '*':
+        star_locations.append(grid[y-1][x-1])
         return True
     if x != 0 and grid[y][x-1] == '*':
+        star_locations.append(grid[y][x-1])
         return True
     if x != 0 and y != maximum and grid[y+1][x-1] == '*':
+        star_locations.append(grid[y+1][x-1])
         return True
     if y != 0 and grid[y-1][x] == '*':
+        star_locations.append(grid[y-1][x])
         return True
     if y != maximum and grid[y+1][x] == '*':
+        star_locations.append(grid[y+1][x])
         return True
     if x != maximum and y != 0 and grid[y-1][x+1] == '*':
+        star_locations.append(grid[y-1][x+1])
         return True
     if x != maximum and grid[y][x+1] == '*':
+        star_locations.append(grid[y][x+1])
         return True
     if x != maximum and y != maximum and grid[y+1][x+1] == '*':
+        star_locations.append(grid[y+1][x+1])
         return True
-    return False
+    return False, star_locations
 
 
 if __name__ == "__main__":
