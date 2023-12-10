aa4398a847f5b2470e115c5f76eb7f6325c9e48f
HarryEP <44836842+HarryEP@users.noreply.github.com>
2023-12-10 21:08:39 +0000

    day 4, part 2 solved for sample.txt

--- 2023/day_4/part_2.py
+++ 2023/day_4/part_2.py
@@ -1,27 +1,29 @@
 from part_1 import read_file
 
 
-def scratchcard_value(scratchcard: str, card_quantity: dict) -> int:
+def calculate_number_of_scratchcards(scratchcard: str, card_quantity: dict, card_no: int) -> dict:
     '''finding scratchcard value for each scratchcard'''
     winning_numbers, card_numbers = scratchcard.split("|")
-    total = 0
+    wins = 0
     for num in winning_numbers.split():
         if num in card_numbers.split():
-            if total == 0:
-                total = 1
-            else:
-                total *= 2
-    return total
+            wins += 1
+    for i in range(card_quantity[card_no]):
+        for i in range(1, wins + 1):
+            try:
+                card_quantity[card_no + i] += 1
+            except:
+                print(f"card {card_no + i} is invalid")
+    return card_quantity
 
 
 def sum_up_scratchcards(data: list[str]) -> int:
     '''counts the total amount of points'''
     card_quant = set_card_quantity_dictionary(len(data))
-    total = 0
-    for scratchcard in data:
+    for i, scratchcard in enumerate(data):
         card = scratchcard.split(":")[1]
-        total += scratchcard_value(card, card_quant)
-    return total
+        card_quant = calculate_number_of_scratchcards(card, card_quant, i + 1)
+    return sum(card_quant.values())
 
 
 def set_card_quantity_dictionary(num_of_cards: int) -> dict:
@@ -30,6 +32,7 @@ def set_card_quantity_dictionary(num_of_cards: int) -> dict:
     for i in range(1, num_of_cards+1):
         card_quantity[i] = 1
     print(card_quantity)
+    return card_quantity
 
 
 if __name__ == "__main__":
