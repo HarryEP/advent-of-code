31fb2399cfb2fb5a3f7eaee917a55acc2f896c5e
HarryEP <harryeplant@gmail.com>
2024-12-01 10:46:31 +0000

    setup

--- 2024/day1/part1.py
+++ 2024/day1/part1.py
@@ -1,1 +1,10 @@
+def read_file(file_name: str):
+    '''file to read file with file_name as given'''
+    with open(file_name) as file:
+        data = file.read()
+    return data.split('\n')
 
+
+if __name__ == "__main__":
+    data = read_file('day_1/sample1.txt')
+    print(data)
